029535c9d4f5e83f7d2d873c7d37a407f57f7b9a
Tejasree-S27 <stejasree348@gmail.com>
2024-12-11 11:28:10 +0530

    Add files via upload

---
+++ Private_StaticMethod.java
@@ -0,0 +1,12 @@
+class Private_StaticMethod
+    {
+        //using private access modifier
+        private static void s()
+        {
+            System.out.println("Good Morning");
+        }
+        public static void main(String[] args)
+        {
+            s();
+        }
+    }
