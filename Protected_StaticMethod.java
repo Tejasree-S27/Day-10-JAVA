9527fc6b5a904369aa6f4e37b28f870f419500ee
Tejasree-S27 <stejasree348@gmail.com>
2024-12-11 11:30:09 +0530

    Add files via upload

---
+++ Protected_StaticMethod.java
@@ -0,0 +1,12 @@
+class Protected_StaticMethod
+    {
+        //using Protected access modifier
+        protected static void s()
+        {
+            System.out.println("Good Afternoon");
+        }
+        public static void main(String[] args)
+        {
+            s();
+        }
+    }
