583687c4065f8b755bab13726fb8ea6a37a90b86
Tejasree-S27 <stejasree348@gmail.com>
2024-12-11 11:26:07 +0530

    Add files via upload

---
+++ PublicStaticMethod.java
@@ -0,0 +1,12 @@
+    class PublicStaticMethod
+    {
+        //using public access modifier
+        public static void s()
+        {
+            System.out.println("Hello");
+        }
+        public static void main(String[] args)
+        {
+            s();
+        }
+    }
